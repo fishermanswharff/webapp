7a91bdc185384df13e667ee4c2ea4b5ba11fd015
fishermanswharff <fishermanswharff@github.com>
2014-11-09 02:06:02 -0500

    ajax get request on app.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function trace(){ for(var i = 0, count = arguments.length; i < count; i++){console.log(arguments[i]);}};
+
+$(document).ready(function(){
+  $.ajax({
+      url: 'http://bobsapi.herokuapp.com/products',
+      type: 'GET',
+      complete: function(jqXHR,textStatus){
+        trace(jqXHR, textStatus, "complete get!!");
+      },
+      success: function(data, textStatus, jqXHR){
+        trace(data,textStatus, jqXHR, "successful get!!");
+      }, 
+      error: function(jqXHR,error,exception){
+        trace(jqXHR,error,exception);
+      },
+    }).done(function(response){
+      trace(response, "done ajax!!");
+    }).fail(function(jqXHR, textStatus, thrownError){
+      trace(jqXHR, textStatus, thrownError);
+      router.navigate("home",{trigger: true});
+    });
+});
