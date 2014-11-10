bba2555c1b58de009707ad1e11cbe04dbf899a78
mattmcdevitt93 <mattmcdevitt93@gmail.com>
2014-11-10 10:45:41 -0500

    menu now pulls from API ONCE and cycles through selected items

--- js/app.js
+++ js/app.js
@@ -1,35 +1,39 @@
+function trace(){ for(var i = 0, count = arguments.length; i < count; i++){console.log(arguments[i]);}}
+
 menu = {};
 
 menu.load_menu = function(data) {
-      data = data.products;
+      trace(data);
+      data = data[menu.selected].products;
       var item = 0;
       data.forEach(function(data) {
         item = item + 1;
-        $('#Front_menu').append($('<div class="wire item id="menu_item_' + item + '">').text(data.title));
+        $('#Front_menu').append($('<div class="wire item id="menu_item_' + item + '">').text(data.title).text(data.description));
         $('#Front_menu').append($('<img src=' + data.image_url + ' class="image">'));
 
       });
   console.log("Menu Loaded");
 };
 
+menu.clear = function() {
+  $("#Front_menu").html("");
+  menu.load_menu(menu.data);
+};
 
-
-function trace(){ for(var i = 0, count = arguments.length; i < count; i++){console.log(arguments[i]);}}
-
-$(document).ready(function(){
-  var selected = 1;
+menu.get_request = function() {
   $.ajax({
-      url: 'https://bobsapi.herokuapp.com/categories/' + selected,
+      url: 'https://bobsapi.herokuapp.com/categories/',
       type: 'GET',
       complete: function(jqXHR,textStatus){
-        trace(jqXHR, textStatus, "complete get!!");
+        // trace(jqXHR, textStatus, "complete get!!");
       },
       success: function(data, textStatus, jqXHR){
-        trace(data,textStatus, jqXHR, "successful get!!");
+        // trace(data,textStatus, jqXHR, "successful get!!");
+        menu.data = (data);
         menu.load_menu(data);
       },
       error: function(jqXHR,error,exception){
-        trace(jqXHR,error,exception);
+        // trace(jqXHR,error,exception);
       },
     }).done(function(response){
       trace(response, "done ajax!!");
@@ -37,8 +41,16 @@ $(document).ready(function(){
       trace(jqXHR, textStatus, thrownError);
       router.navigate("home",{trigger: true});
     });
+};
+
+// --------------------------------------------------------------
+
+$(document).ready(function(){
+  menu.selected = 0;
+  menu.get_request();
 });
 
 $( ".selected" ).click(function() {
-  alert(event.target.id);
+  menu.selected = event.target.id;
+  menu.clear();
 });
